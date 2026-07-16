f7aa7bc990f0914a8866f340b69049c40d5737a8
nehakore <nehakore2121@gmail.com>
2026-07-16 17:08:18 +0530

    nothing to commit

--- src/main/java/in/sp/main/JobPortalApplication.java
+++ src/main/java/in/sp/main/JobPortalApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class JobPortalApplication extends SpringBootServletInitializer {
 
@@ -13,7 +16,13 @@ public class JobPortalApplication extends SpringBootServletInitializer {
         return application.sources(JobPortalApplication.class);
     }
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
+
 	public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(JobPortalApplication.class, args);
 		System.out.println("Job Search platform started");
 	}
